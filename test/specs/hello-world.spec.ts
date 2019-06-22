b5fb31df34d09350c5a6da2737379a5c9e973fd5
Mystilund <development.xplx@gmail.com>
2019-06-22 20:07:32 +0200

    Remove Jest and install Mocha/Chai/Nyc

---
+++ test/specs/hello-world.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai'
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+describe('Test function', () => {
+  it('test', () => {
+    expect(true).to.equal(true)
+  });
+});
